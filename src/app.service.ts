1d7f8b983f0bcc26ca69c62dd1de5a54ddf39b23
Pierre-Henry Soria <pierre@ph7.me>
2023-10-21 12:26:41 +1100

    Read version from package json file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,13 @@
+import * as path from 'path';
+import * as fs from 'fs';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getVersion(): string {
-    const version = '1.0.0';
+    const baseDir = process.cwd() + '/';
+    const pathToPackage = path.join(baseDir, 'package.json');
+    const { version } = JSON.parse(fs.readFileSync(pathToPackage).toString());
 
     return version;
   }
