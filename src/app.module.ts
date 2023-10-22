da9acd3ec0bbfc4000bf31a7d7f587d80419b8db
Pierre-Henry Soria <pierre@ph7.me>
2023-10-22 16:32:24 +1100

    Update ENV TypeORM option values

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,8 @@ const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  entities: [__dirname + process.env.TYPEORM_ENTITIES],
+  synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
+  entities: ['dist/**/*.entity{.ts,.js}'],
 };
 
 @Module({
